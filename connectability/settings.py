cba61a34ccafe9bbab339d9b5b2eed213567329d
RUTH ATIENO <ruthatieno561@yahoo.com>
2025-12-14 14:08:38 +0300

    Update settings.py: register apps and configure media files

--- connectability/settings.py
+++ connectability/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'users_app',
+    'posts_app',
+    'follows_app',
+
+    # Third-party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
